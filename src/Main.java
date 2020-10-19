e7927bb03a8845ec05b152d5d4a152b1f4dbfac9
Dawid Obrebski <herligho232@gmail.com>
2020-10-19 16:36:54 +0200

    Dodanie metody ktora zlicza ilosc unikalnych nazwisk

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ class Main {
         PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
 
         System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
     }
 }
 
@@ -29,6 +30,12 @@ class PeopleStats {
     }
     public long count() {
         return people.size();
+    }
+    public long countUniqueLastNames() {
+        return people.stream()
+                .map(Person::getLastName)
+                .distinct()
+                .count();
     }
 }
 
